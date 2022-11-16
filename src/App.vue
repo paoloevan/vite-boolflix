f267dcac86139b1f59fdb05129c02f9eab78f483
paoloevan <paoloev1900@gmail.com>
2022-11-16 16:04:35 +0100

    add milestone 2 (flags language and tv show search)

--- src/App.vue
+++ src/App.vue
@@ -20,14 +20,19 @@ export default {
     <input v-model="store.query" type="search" placeholder="Digita film">
     <button @click="this.store.callApi()">Cerca</button>
     <ul>
-      <li v-for="film in store.searchFilm">Titolo: {{ film.title }}, Titolo originale: {{ film.original_title }}, lingua
+      <li v-for="film in store.searchFilm">Titolo: {{ film.name }} {{ film.title }}, Titolo originale: {{
+          film.original_title
+      }}{{ film.original_name }}, lingua
         originale:
         <span>
           <img width="20" :src="film.original_language === 'en' ? '../public/Flag_of_the_United_Kingdom.svg.png' : ''"
             alt="">
           <img width="20" :src="film.original_language === 'it' ? '../public/Flag_of_Italy.svg.png' : ''" alt="">
           <img width="20" :src="film.original_language === 'es' ? '../public/Flag_of_Spain.svg.png' : ''" alt="">
-        </span>,
+        </span> <span
+          v-if="film.original_language !== 'en' && film.original_language !== 'it' && film.original_language !== 'es'">{{
+              film.original_language
+          }}</span>,
         voto: {{ film.vote_average }}
       </li>
     </ul>
--- src/store.js
+++ src/store.js
@@ -2,7 +2,9 @@ import { reactive } from 'vue'
 import axios from 'axios'
 
 export const store = reactive({
-    API_url: 'https://api.themoviedb.org/3/search/movie?',
+    API_url: 'https://api.themoviedb.org/3/search/multi?',
+    // API_movie: 'movie?',
+    // API_tv: 'tv?',
     API_key: 'api_key=e6a92130dd300f6d7b60f8e64ed7292c',
     query: '',
     searchFilm: '',
@@ -12,6 +14,7 @@ export const store = reactive({
         axios.get(url)
             .then(response => {
                 this.searchFilm = response.data.results
+                console.log(response.data.results);
             })
 
     }
