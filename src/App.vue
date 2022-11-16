4faf151f804d1ef281632247e8ad875d0da6bf01
paoloevan <paoloev1900@gmail.com>
2022-11-16 13:23:13 +0100

    add milestone 1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
 <script>
+import { store } from './store.js'
+
+export default {
+  name: 'App',
+  components: store,
+  data() {
+    return {
+      store
+    }
+  }
+}
 </script>
 
 <template>
-
+  <div class="search">
+    <input v-model="store.query" type="search" placeholder="Digita film">
+    <button @click="this.store.callApi()">Cerca</button>
+    <ul>
+      <li v-for="film in store.searchFilm">Titolo: {{ film.title }}, Titolo originale: {{ film.original_title }}, lingua
+        originale: {{ film.original_language }},
+        voto: {{ film.vote_average }}</li>
+    </ul>
+  </div>
 </template>
 
 <style>
--- src/store.js
+++ src/store.js
@@ -1,7 +1,19 @@
 import { reactive } from 'vue'
+import axios from 'axios'
 
 export const store = reactive({
     API_url: 'https://api.themoviedb.org/3/search/movie?',
     API_key: 'api_key=e6a92130dd300f6d7b60f8e64ed7292c',
-    query: ''
-})
+    query: '',
+    searchFilm: '',
+
+    callApi() {
+        const url = this.API_url + this.API_key + '&query=' + this.query
+        axios.get(url)
+            .then(response => {
+                this.searchFilm = response.data.results
+                console.log(this.searchFilm);
+            })
+    }
+}
+)
