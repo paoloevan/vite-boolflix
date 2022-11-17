5ffb0cb9c94ec340982a509d95f20a4edbd3b57a
paoloevan <paoloev1900@gmail.com>
2022-11-17 11:13:36 +0100

    add AppMain component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script>
 import { store } from './store.js'
 import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
 
 export default {
   name: 'App',
   components: {
-    AppHeader
+    AppHeader,
+    AppMain
   },
   data() {
     return {
@@ -19,41 +21,9 @@ export default {
 
   <AppHeader />
   <!-- / header -->
-  <div class="search">
-    <input @keyup.enter="this.store.callApi()" v-model="store.query" type="search" placeholder="Digita film">
-    <button @click="this.store.callApi()">Cerca</button>
-  </div>
-  <ul>
-    <template v-for="film in store.searchFilm">
-      <!-- film item -->
-      <li v-if="film.media_type !== 'person'">
-        <img :src="store.API_image + film.poster_path" :alt="film.title + film.name">
-        Titolo: {{ film.name }} {{ film.title }},
-        Titolo
-        originale: {{
-            film.original_title
-        }}{{ film.original_name }}, lingua
-        originale:
-        <span>
-          <!-- bandierina lingue -->
-          <img width="20" :src="film.original_language === 'en' ? '../public/Flag_of_the_United_Kingdom.svg.png' : ''"
-            alt="">
-          <img width="20" :src="film.original_language === 'fr' ? '../public/Flag_of_France.svg.png' : ''" alt="">
-          <img width="20" :src="film.original_language === 'it' ? '../public/Flag_of_Italy.svg.png' : ''" alt="">
-          <img width="20" :src="film.original_language === 'es' ? '../public/Flag_of_Spain.svg.png' : ''" alt="">
-        </span>
-        <span
-          v-if="film.original_language !== 'en' && film.original_language !== 'it' && film.original_language !== 'es' && film.original_language !== 'fr'">{{
-              film.original_language
-          }}</span>
-        <!-- stelline voto -->
-        <div v-for="n in store.convertRating(film.vote_average)"><i class="fa-solid fa-star"></i></div>
-        <div v-for="n in 5 - store.convertRating(film.vote_average)"><i class="fa-regular fa-star"></i></div>
+  <AppMain />
+  <!-- / main -->
 
-      </li>
-    </template>
-
-  </ul>
 </template>
 
 <style>
