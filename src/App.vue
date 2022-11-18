b85e58bd9bbf8c00a304ae902dc062996d5bee67
paoloevan <paoloev1900@gmail.com>
2022-11-18 11:52:11 +0100

    add AppFooter component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script>
 import AppHeader from './components/AppHeader.vue'
 import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
 
 export default {
   name: 'App',
   components: {
     AppHeader,
-    AppMain
+    AppMain,
+    AppFooter
   }
 }
 </script>
@@ -17,6 +19,8 @@ export default {
   <!-- / header -->
   <AppMain />
   <!-- / main -->
+  <AppFooter />
+  <!-- / footer -->
 
 </template>
 
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: 'AppFooter'
+}
+</script>
+
+<template>
+    <footer>
+        <div>
+            Made with &#10084;&#65039; by Paoloevan
+        </div>
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+    color: white;
+    text-align: center;
+    font-size: 0.75rem;
+    padding: 1rem;
+}
+</style>
